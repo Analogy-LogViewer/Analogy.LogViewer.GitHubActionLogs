aba411a96ec7f181ba4f6bca10f461dde7b5580f
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-12-02 13:31:48 +0200

    update nugets

--- Analogy.LogViewer.GitHubActionLogs/Parser/GitHubActionPlainTextLogFileLoader.cs
+++ Analogy.LogViewer.GitHubActionLogs/Parser/GitHubActionPlainTextLogFileLoader.cs
@@ -57,9 +57,13 @@ namespace Analogy.LogViewer.GitHubActionLogs.Parser
                             else if (entry != null)
                             {
                                 if (entry.Text == "")
+                                {
                                     entry.Text = line;
+                                }
                                 else
+                                {
                                     entry.Text += Environment.NewLine + line;
+                                }
                             }
                         }
                     }
